3cac1e0aef94bec3a82a1af712fd426fa710743d
Daniel <danielzx9@gmail.com>
2018-11-09 16:22:19 -0500

    listado de vehiculos
    echo el listado de vehiculos

--- Vistas/vehiculos/listar.php
+++ Vistas/vehiculos/listar.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Listado de vehiculos</title>
 </head>
 <body>
-
+	 <a href="index.php?c=vehiculos&a=create">Agregar</a>
+    
+	<table border="2">            
+		<tbody>
+			<tr>
+				<th>Id</th>
+				<th>Placa</th>
+				<th>Marca</th>
+				<th>Servicio</th>
+				<th>Registro</th>
+				<th>Acciones</th>
+			</tr>
+			<?php foreach($vehiculos as $vehiculos){ ?>
+			<tr>
+				<td><?= $vehiculos->id_cliente; ?></td>
+				<td><?= $vehiculos->placa; ?></td>
+				<td><?= $vehiculos->marca; ?></td>
+				<td><?= $vehiculos->servicio; ?></td>
+				<td><?= $vehiculos->registro; ?></td>
+				<td> 
+                    <a href="index.php?c=vehiculos&a=update&id=<?=$vehiculos->id_cliente ?>">Editar</a> 
+                    <a href="index.php?c=vehiculos&a=delete&id=<?=$vehiculos->id_cliente ?>">Eliminar</a>
+                </td>
+			</tr>
+			<?php } ?>
 </body>
 </html>
