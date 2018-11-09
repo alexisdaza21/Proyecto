394e476209d8dbc12bbcbeeae8d2d6672e516d45
Daniel <danielzx9@gmail.com>
2018-11-09 16:12:41 -0500

    diseño vehiculos
    diseño para poder crear un vehiculo

--- Vistas/vehiculos/create.php
+++ Vistas/vehiculos/create.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Crear vehiculos</title>
 </head>
 <body>
+	<form action="" method="post">
+		<label>Placa:</label>
+		<input   type="text"   name="vehiculos[placa]" value="" required=""/>	
+		
+		<label>Marca:</label>
+		<input type="text" name="vehiculos[marca]" value="" required=""/>
 
+		<label>Servicio:</label>
+		<select name="vehiculos[servicio]">
+			<option value="Publico">Publico</option>
+			<option value="Particular">Particular</option>
+		</select>
+
+		<label>Registro:</label>
+		<input type="date" name="vehiculos[registro]" value="" required=""/>
+
+		<button type="submit">Crear vehiculos</button>
+	</form>
 </body>
 </html>
